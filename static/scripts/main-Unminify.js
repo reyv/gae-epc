f390c3683a609104a73e6ee7b85fd0a349087bdb
Rey Villa <reyv10@gmail.com>
2012-09-21 15:29:55 -0400

    Client Side Form Validation: 1) Reduced jQuery 2)Validation completely works.

--- static/scripts/main-Unminify.js
+++ static/scripts/main-Unminify.js
@@ -1,5 +1,4 @@
 //Main Page
-
 $(document).ready(function (){
 	beta = $('#text_box_beta').val();
 	alpha = $('#text_box_alpha').val();
@@ -12,49 +11,49 @@ $(document).ready(function (){
 	}
 });
 
-function validate(elem, id, min, max){
+function validate($obj, min, max){
+	elem = $obj.val();
 	var numericExpression = /^[.0-9]+$/;
 	
   	if(elem.match(numericExpression)){
 	  if(elem >= min && elem <= max){
-	    $(id).css("color","#3bd2e3");
-	    $(id).html("OK"); 
+		$obj.parent().next().text('OK');
 	  }  
 	}else{
-	    $(id).css("color",red);
-	    $(id).html("ERROR");
-	}    
+		$obj.parent().next().css('color','red').text('ERROR');
+	}
 }
 
 $('#text_box_beta').change(function (){
-  var beta = $(this).val();
+  $this = $(this);
+  var beta = $this.val();
   var alpha = $('#text_box_alpha').val();
-  
   drawWall(beta,alpha);
-  validate(beta,'#verify_beta',60,90)
+  validate($this,60,90);
 });
 
 $('#text_box_phi').change(function (){
-  var value = $(this).val();
-  validate(value,'#verify_phi',20,45);
+  $this = $(this);
+  validate($this,20,45);
 });
 
 $('#text_box_alpha').change(function (){
+  $this = $(this);
   var beta = $('#text_box_beta').val();
-  var alpha = $(this).val();
+  var alpha = $this.val();
 
   drawWall(beta,alpha);
-  validate(alpha,'#verify_alpha',0,25)
+  validate($this,0,25)
 });
 
 $('#text_box_delta').change(function (){
-  var value = $(this).val();
-  validate(value,'#verify_delta',0,30);
+  $this = $(this);
+  validate($this,0,30);
 });
 
 $('#text_box_ocr').change(function (){
-  var value = $(this).val();
-  validate(value,'#verify_ocr',0,5);
+  $this = $(this);
+  validate($this,0,5);
 });
 
 //About Page
